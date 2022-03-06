b59252a5fe957904124f0d7d62771951ef6db2c9
Kumail Raza <46721070+kumailispro@users.noreply.github.com>
2022-03-06 05:15:11 -0500

    added popupmenu

--- src/Assignments/A5/OptionFrame.java
+++ src/Assignments/A5/OptionFrame.java
@@ -3,8 +3,7 @@ package Assignments.A5;
 import javax.swing.*;
 import java.awt.*;
 
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
 import java.io.*;
 import java.util.ArrayList;
 
@@ -15,11 +14,12 @@ public class OptionFrame extends JFrame {
     private static JRadioButton addArc;
     private static JRadioButton removeArc;
     private static ButtonGroup optionGroup;
-    private static JButton resetButton;
-    private static JButton saveButton;
-    private static JButton loadButton;
 
     private static DrawFrame drawFrame;
+
+    // popupmenu items
+    private final JPopupMenu popupMenu;
+    public MenuItemListener menuItemListener;
 
     public OptionFrame(){
         // default size/layout settings for OptionFrame
@@ -36,11 +36,11 @@ public class OptionFrame extends JFrame {
         drawFrame = new DrawFrame();
 
         // initialize RadioButtons
-        addNode = new JRadioButton("Add Node", true);
-        deleteNode = new JRadioButton("Delete Node", false);
-        moveNode = new JRadioButton("Move Node", false);
-        addArc = new JRadioButton("Add Arc", false);
-        removeArc = new JRadioButton("Remove Arc", false);
+        addNode = new CustomRadioButton("Add Node", true);
+        deleteNode = new CustomRadioButton("Delete Node", false);
+        moveNode = new CustomRadioButton("Move Node", false);
+        addArc = new CustomRadioButton("Add Arc", false);
+        removeArc = new CustomRadioButton("Remove Arc", false);
 
         // add RadioButtons to OptionFrame
         add(addNode);
@@ -65,25 +65,92 @@ public class OptionFrame extends JFrame {
         removeArc.addItemListener(new RadioButtonHandler("removeArc"));
 
         // reset button
-        resetButton = new JButton("Reset");
+        JButton resetButton = new CustomButton("Reset");
         add(resetButton);
         resetButton.addActionListener(e -> { // activates on button press
             reset();
         });
 
         // save button
-        saveButton = new JButton("Save to File");
+        JButton saveButton = new CustomButton("Save to File");
         add(saveButton);
         saveButton.addActionListener(e -> {
             save(); // save to file
         });
 
         // load button
-        loadButton = new JButton("Load From File");
+        JButton loadButton = new CustomButton("Load From File");
         add(loadButton);
         loadButton.addActionListener(e -> {
             load(); // load from file
         });
+
+        // initialize popupmenu
+        popupMenu = new JPopupMenu();
+
+        // initialize menuItemListener
+        menuItemListener = new MenuItemListener();
+
+        // initialize addNode menu item
+        JMenuItem setAddNode = new JMenuItem("Add Node");
+        setAddNode.setActionCommand("addNode");
+        setAddNode.addActionListener(menuItemListener);
+        popupMenu.add(setAddNode);
+
+        // initialize deleteNode menu item
+        JMenuItem setDeleteNode = new JMenuItem("Delete Node");
+        setDeleteNode.setActionCommand("removeNode");
+        setDeleteNode.addActionListener(menuItemListener);
+        popupMenu.add(setDeleteNode);
+
+        // initialize moveNode menu item
+        JMenuItem setMoveNode = new JMenuItem("Move Node");
+        setMoveNode.setActionCommand("moveNode");
+        setMoveNode.addActionListener(menuItemListener);
+        popupMenu.add(setMoveNode);
+
+        // initialize addArc menu item
+        JMenuItem setAddArc = new JMenuItem("Add Arc");
+        setAddArc.setActionCommand("addArc");
+        setAddArc.addActionListener(menuItemListener);
+        popupMenu.add(setAddArc);
+
+        // initialize deleteNode menu item
+        JMenuItem setDeleteArc = new JMenuItem("Delete Arc");
+        setDeleteArc.setActionCommand("removeArc");
+        setDeleteArc.addActionListener(menuItemListener);
+        popupMenu.add(setDeleteArc);
+
+        // initialize reset menu item
+        JMenuItem resetOption = new JMenuItem("Reset Draw Frame");
+        resetOption.setActionCommand("reset");
+        resetOption.addActionListener(menuItemListener);
+        popupMenu.add(resetOption);
+
+        // initialize save menu item
+        JMenuItem saveOption = new JMenuItem("Save Graph to File");
+        saveOption.setActionCommand("save");
+        saveOption.addActionListener(menuItemListener);
+        popupMenu.add(saveOption);
+
+        // initialize load menu item
+        JMenuItem loadOption = new JMenuItem("Load Graph from File");
+        loadOption.setActionCommand("load");
+        loadOption.addActionListener(menuItemListener);
+        popupMenu.add(loadOption);
+
+        // show on right click
+        OptionFrame temp = this;
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e)) // make sure it is a right click
+                    popupMenu.show(temp, e.getX(),e.getY());
+            }
+        });
+
+        // add popupmenu to frame
+        add(popupMenu);
 
         // make visible
         setVisible(true);
@@ -160,4 +227,144 @@ public class OptionFrame extends JFrame {
             if (e.getStateChange() == ItemEvent.SELECTED) DrawFrame.drawPanel.setMode(option);
         }
     }
+    public static class MenuItemListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            switch(e.getActionCommand()){
+                case "addNode":
+                    optionGroup.clearSelection();
+                    addNode.setSelected(true);
+                    DrawFrame.drawPanel.setMode(e.getActionCommand());
+                    break;
+                case "removeNode":
+                    optionGroup.clearSelection();
+                    deleteNode.setSelected(true);
+                    DrawFrame.drawPanel.setMode(e.getActionCommand());
+                    break;
+                case "moveNode":
+                    optionGroup.clearSelection();
+                    moveNode.setSelected(true);
+                    DrawFrame.drawPanel.setMode(e.getActionCommand());
+                    break;
+                case "addArc":
+                    optionGroup.clearSelection();
+                    addArc.setSelected(true);
+                    DrawFrame.drawPanel.setMode(e.getActionCommand());
+                    break;
+                case "removeArc":
+                    optionGroup.clearSelection();
+                    removeArc.setSelected(true);
+                    DrawFrame.drawPanel.setMode(e.getActionCommand());
+                    break;
+                case "reset":
+                    reset();
+                    break;
+                case "save":
+                    save();
+                    break;
+                case "load":
+                    load();
+                    break;
+                default:
+                    System.out.println("SOMETHING BROKE - the current ActionCommand: " + e.getActionCommand());
+            }
+        }
+    }
 }
+class CustomButton extends JButton {
+    private JPopupMenu popupMenu;
+    public CustomButton() {
+        generate();
+    }
+    public CustomButton(String text){
+        super(text);
+        generate();
+    }
+    private void generate(){
+        // popupmenu
+        popupMenu = new JPopupMenu();
+        // menuitemlistener
+        OptionFrame.MenuItemListener menuItemListener = new OptionFrame.MenuItemListener();
+
+        // reset
+        JMenuItem resetOption = new JMenuItem("Reset Draw Frame");
+        resetOption.setActionCommand("reset");
+        resetOption.addActionListener(menuItemListener);
+        popupMenu.add(resetOption);
+
+        // save
+        JMenuItem saveOption = new JMenuItem("Save Graph to File");
+        saveOption.setActionCommand("save");
+        saveOption.addActionListener(menuItemListener);
+        popupMenu.add(saveOption);
+
+        // load
+        JMenuItem loadOption = new JMenuItem("Load Graph from File");
+        loadOption.setActionCommand("load");
+        loadOption.addActionListener(menuItemListener);
+        popupMenu.add(loadOption);
+
+        // add click registration
+        CustomButton temp = this;
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e)) // make sure it is a right click
+                    popupMenu.show(temp, e.getX(),e.getY());
+            }
+        });
+    }
+}
+
+class CustomRadioButton extends JRadioButton {
+    private JPopupMenu popupMenu;
+    public CustomRadioButton(String text, boolean selected){
+        super(text, selected);
+        generate();
+    }
+    public void generate(){
+        popupMenu = new JPopupMenu();
+        OptionFrame.MenuItemListener menuItemListener = new OptionFrame.MenuItemListener();
+
+        // initialize addNode menu item
+        JMenuItem setAddNode = new JMenuItem("Add Node");
+        setAddNode.setActionCommand("addNode");
+        setAddNode.addActionListener(menuItemListener);
+        popupMenu.add(setAddNode);
+
+        // initialize deleteNode menu item
+        JMenuItem setDeleteNode = new JMenuItem("Delete Node");
+        setDeleteNode.setActionCommand("removeNode");
+        setDeleteNode.addActionListener(menuItemListener);
+        popupMenu.add(setDeleteNode);
+
+        // initialize moveNode menu item
+        JMenuItem setMoveNode = new JMenuItem("Move Node");
+        setMoveNode.setActionCommand("moveNode");
+        setMoveNode.addActionListener(menuItemListener);
+        popupMenu.add(setMoveNode);
+
+        // initialize addArc menu item
+        JMenuItem setAddArc = new JMenuItem("Add Arc");
+        setAddArc.setActionCommand("addArc");
+        setAddArc.addActionListener(menuItemListener);
+        popupMenu.add(setAddArc);
+
+        // initialize deleteNode menu item
+        JMenuItem setDeleteArc = new JMenuItem("Delete Arc");
+        setDeleteArc.setActionCommand("removeArc");
+        setDeleteArc.addActionListener(menuItemListener);
+        popupMenu.add(setDeleteArc);
+
+        // add click registration
+        CustomRadioButton temp = this;
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e)) // make sure it is a right click
+                    popupMenu.show(temp, e.getX(),e.getY());
+            }
+        });
+    }
+}
+
