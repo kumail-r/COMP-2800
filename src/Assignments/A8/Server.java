77bfd488bc9ddfd1586ac3bc75f4b1ab4bcb11e6
Kumail Raza <46721070+kumailispro@users.noreply.github.com>
2022-03-27 12:12:58 -0400

    started A8

---
+++ src/Assignments/A8/Server.java
@@ -0,0 +1,11 @@
+package Assignments.A8;
+
+public class Server {
+    public static void main (String[] args){
+        if (args.length == 0){
+            System.out.println("No file names provided.\nExiting program.");
+            return;
+        }
+
+    }
+}
